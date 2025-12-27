b7238ccfa0e69f49c3740b9d84d9fd0756e1bf05
priscarautomoveis <priscarautomoveis@gmail.com>
2025-12-27 23:24:45 +0000

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
+const menuBtn = document.getElementById("menuBtn");
+const sidebar = document.getElementById("sidebar");
+const overlay = document.getElementById("overlay");
+
+menuBtn.addEventListener("click", () => {
+  sidebar.classList.add("active");
+  overlay.classList.add("active");
+});
+
+overlay.addEventListener("click", () => {
+  sidebar.classList.remove("active");
+  overlay.classList.remove("active");
+});
 
