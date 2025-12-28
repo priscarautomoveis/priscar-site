2440fa992385e68b081a1d59f2668556e411f3aa
priscarautomoveis <priscarautomoveis@gmail.com>
2025-12-28 15:40:56 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,23 @@
-const menuBtn = document.getElementById("menuBtn");
-const sidebar = document.getElementById("sidebar");
-const overlay = document.getElementById("overlay");
+document.querySelectorAll(".car-card").forEach(card => {
+  const slider = card.querySelector(".photo-slider");
+  const imgs = slider.querySelectorAll("img");
+  const left = card.querySelector(".arrow.left");
+  const right = card.querySelector(".arrow.right");
+  let index = 0;
 
-menuBtn.addEventListener("click", () => {
-  sidebar.classList.toggle("active");
-  overlay.classList.toggle("active");
+  const update = () => {
+    imgs.forEach((img, i) => img.classList.toggle("active", i === index));
+    left.classList.toggle("disabled", index === 0);
+    right.classList.toggle("disabled", index === imgs.length - 1);
+  }
 
-  menuBtn.classList.toggle("open");
-});
+  left.addEventListener("click", () => {
+    if (index > 0) index--;
+    update();
+  });
 
-overlay.addEventListener("click", () => {
-  sidebar.classList.remove("active");
-  overlay.classList.remove("active");
+  right.addEventListener("click", () => {
+    if (index < imgs.length - 1) index++;
+    update();
+  });
 });
